3c1b25faf3125ee332f27b22c5d9826ff7478cbf
Alvin <alvin.chen.718@gmail.com>
2025-04-14 18:16:29 -0400

    Add cloudfront distribution outputs for deployment workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -117,3 +117,19 @@ resource "aws_cloudfront_distribution" "website_cdn" {
     Environment = var.environment
   }
 }
+
+# Outputs - Add these to the end of your main.tf file
+output "website_bucket_name" {
+  description = "Name of the S3 bucket used for website hosting"
+  value       = aws_s3_bucket.website_bucket.id
+}
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.website_cdn.id
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.website_cdn.domain_name
+}
