affa8d86065b007c3b223595092e0a3d8e0d7ffe
Alvin <alvin.chen.718@gmail.com>
2025-04-14 19:52:28 -0400

    Add aws profile configuration for pomodoro

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,19 +5,20 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
-backend "s3" {
-  bucket         = "ac-pomodoro-terraform-state"
-  key            = "terraform.tfstate"
-  region         = "us-east-1"
-  encrypt        = true
-  dynamodb_table = "terraform-lock"
-}
+
+  backend "s3" {
+    bucket         = "ac-pomodoro-terraform-state"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
 
 provider "aws" {
-  region = var.aws_region
-  
+  region  = var.aws_region
+  profile = "pomodoro"
+
   # Use these tags for all resources that support them
   default_tags {
     tags = {
